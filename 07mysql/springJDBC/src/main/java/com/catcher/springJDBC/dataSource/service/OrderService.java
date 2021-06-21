9a94aca535501d9c6945b4ee9cf9f595d45dec1f
catcherwyf <catcherwyf@outlook.com>
2021-06-21 21:54:06 +0800

    第七周作业 10 读写分离 - 数据库 shardingSphere

--- 07mysql/springJDBC/src/main/java/com/catcher/springJDBC/dataSource/service/OrderService.java
+++ 07mysql/springJDBC/src/main/java/com/catcher/springJDBC/dataSource/service/OrderService.java
@@ -8,12 +8,16 @@ import com.sun.tools.corba.se.idl.constExpr.Or;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+import javax.sql.DataSource;
 import java.util.List;
 @Service
 public class OrderService {
 
     @Autowired
     OrderInfoMapper mapper;
+    @Resource
+    private DataSource dataSource;
 
     public OrderInfo getOrderInfoById(Long order_id){
         //切换数据源
@@ -26,5 +30,11 @@ public class OrderService {
 
     public void saveOrderInfo(OrderInfo orderInfo){
         mapper.insert(orderInfo);
+    }
+
+
+    public OrderInfo getOrderInfoByOrderId(Long order_id){
+        OrderInfo orderInfo = mapper.selectByPrimaryKey(order_id);
+        return orderInfo;
     }
 }
