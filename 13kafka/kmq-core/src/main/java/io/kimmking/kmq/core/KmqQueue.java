d225869dc53e09f06b1c8a32c2907d19b400e4fc
catcherwyf <catcherwyf@outlook.com>
2021-08-05 22:14:40 +0800

    自定义queue

---
+++ 13kafka/kmq-core/src/main/java/io/kimmking/kmq/core/KmqQueue.java
@@ -0,0 +1,44 @@
+package io.kimmking.kmq.core;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class KmqQueue {
+
+
+    private Map<String, AtomicInteger> offset = new HashMap<>();
+
+    private int writeIndex = 0;
+
+    private List<KmqMessage> queue = new ArrayList<>();
+
+    public boolean put(KmqMessage message){
+        try {
+            queue.add(message);
+            writeIndex++;
+        }catch (OutOfMemoryError error){
+            error.toString();
+            return false;
+        }
+        return true;
+    }
+
+    public KmqMessage get(String group){
+        int index = offset.getOrDefault(group, new AtomicInteger(-1)).incrementAndGet();
+        if (writeIndex == 0 || index >= queue.size()){
+            return null;
+        }
+        return queue.get(index);
+    }
+
+    public boolean isEmpty() {
+        return writeIndex == 0 || writeIndex >= queue.size();
+    }
+
+    public int size() {
+        return writeIndex;
+    }
+}
